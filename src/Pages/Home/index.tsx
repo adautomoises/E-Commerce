964787fa5eda66a0dd42c518cfa8c8876f219a72
adautomoises <adautomaleandro@gmail.com>
2023-03-30 15:04:10 -0300

    Primeiro Commit Página Inicial

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,120 +1,20 @@
-import { useState } from "react";
-import { api } from "../../Services/api";
-import { useNavigate } from "react-router-dom";
-
-import {
-  Container,
-  ContainerAnimation,
-  ContainerBlueIcon,
-  ContainerWhiteIcon,
-  ContainerForm,
-} from "./styles";
-import { AxiosError } from "axios";
-
-import { ReactComponent as BlueIconSvg } from "../../Assets/blue_icon.svg";
-import { ReactComponent as WhiteIconSvg } from "../../Assets/white_icon.svg";
-import { ButtonComponent } from "../../Components/ButtonComponent";
-
-interface IErrorResponse {
-  message: string;
-}
+import { HeaderComponent } from '../../Components/HeaderComponent';
+import { Content, PosterImage, PosterTitle, PromotionalPoster } from './styles';
+import { Body } from './styles';
+import promotionImage from '../../Assets/images/PromotionImage.png';
+import { FloatCard } from '../../Components/FloatCard';
 
 export function Home() {
-  const navigate = useNavigate();
-
-  const isAuth = localStorage.getItem("@e-commerce:token");
-  const [roles, setRoles] = useState("");
-
-  const setRole = () => {
-    api
-      .get("/account/role", {
-        params: {
-          role: roles,
-        },
-      })
-      .then((response) => {
-        alert(response.data);
-      })
-      .catch((error: AxiosError<IErrorResponse>) => {
-        if (error.response) {
-          alert(error.response.data.message);
-        }
-      });
-  };
-  const getUsers = () => {
-    api
-      .get("/account/get")
-      .then((response) => {
-        alert(response.data);
-      })
-      .catch((error: AxiosError<IErrorResponse>) => {
-        if (error.response) {
-          alert(error.response.data.message);
-        }
-      });
-  };
-  const logOut = () => {
-    localStorage.removeItem("@e-commerce:token");
-    window.location.reload();
-  };
-
   return (
-    <Container>
-      <ContainerAnimation className="center">
-        <ContainerBlueIcon>
-          <BlueIconSvg />
-        </ContainerBlueIcon>
-        <ContainerWhiteIcon>
-          <WhiteIconSvg />
-        </ContainerWhiteIcon>
-        <ContainerForm>
-          <div
-            className={"center"}
-            style={{
-              width: "100%",
-              flexDirection: "column",
-              gap: "1rem",
-            }}
-          >
-            {isAuth ? (
-              <>
-                <select
-                  defaultValue={"ADMIN"}
-                  onChange={(event) => {
-                    setRoles(event.target.value);
-                  }}
-                >
-                  <option value={"ADMIN"}>ADMIN</option>
-                  <option value={"BRAND"}>BRAND</option>
-                  <option value={"USER"}>USER</option>
-                </select>
-                <button onClick={setRole}>ADMINISTRAR</button>
-                <button onClick={getUsers}>USUARIOS</button>
-                <button onClick={logOut}>DESLOGAR</button>
-              </>
-            ) : (
-              <>
-                <ButtonComponent
-                  color={"primary"}
-                  type={"button"}
-                  label={"LOGIN"}
-                  onClick={() => {
-                    navigate("/login");
-                  }}
-                />
-                <ButtonComponent
-                  color={"primary"}
-                  type={"button"}
-                  label={"CADASTRAR"}
-                  onClick={() => {
-                    navigate("/register");
-                  }}
-                />
-              </>
-            )}
-          </div>
-        </ContainerForm>
-      </ContainerAnimation>
-    </Container>
+    <Body>
+      <HeaderComponent />
+      <Content>
+        <PromotionalPoster>
+          <PosterImage src={promotionImage} alt="Cartaz Promocional" />
+          <PosterTitle>50% de desconto</PosterTitle>
+          <FloatCard />
+        </PromotionalPoster>
+      </Content>
+    </Body>
   );
 }
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,45 +1,30 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Body = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: var(--blue);
-  position: relative;
-  overflow: hidden;
-`;
-
-export const ContainerAnimation = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%) translateZ(0);
-  width: 100px;
-  height: 100px;
+  overflow-x: hidden;
   background-color: var(--white);
-  border-radius: 2rem;
-  animation: expand-container 1s ease-in-out forwards;
+
+  button {
+    border: none;
+    background: none;
+  }
 `;
 
-export const ContainerBlueIcon = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%) translateZ(0);
-  animation: container-blue 1s ease-in-out forwards;
-`;
-export const ContainerWhiteIcon = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%) translateZ(0);
-  border: 2px solid white;
-  border-radius: 2rem;
-  animation: container-white 1s ease-in-out forwards;
-`;
+export const Content = styled.div``;
 
-export const ContainerForm = styled.div`
-  width: 100%;
-  padding: 1rem;
-  opacity: 0;
-  animation: container-form 1s ease-in-out 500ms forwards;
+export const PromotionalPoster = styled.div`
+  position: relative;
+`;
+export const PosterImage = styled.img`
+  resize: 'cover';
+`;
+export const PosterTitle = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 5%;
+  font-size: 48px;
+  font-weight: bold;
+  color: var(--white);
 `;
